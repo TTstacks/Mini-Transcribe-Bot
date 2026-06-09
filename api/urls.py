cf807baf23d3d0117622c4d7b15aacb7aea470cb
TTstacks <ekosh21.04.2007@gmail.com>
2026-06-09 13:39:19 +0500

    changed auth refresh url typo

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path("auth/register", RegisterView.as_view(), name="auth-register"),
     path("auth/login", LoginView.as_view(), name="auth-login"),
-    path("aut/refresh", TokenRefreshView.as_view(), name="auth-refresh"),
+    path("auth/refresh", TokenRefreshView.as_view(), name="auth-refresh"),
     path("jobs", JobCreateView.as_view(), name="job-create"),
     path("jobs/<int:pk>", JobDetailView.as_view(), name="job-detail"),
     path("me/usage", UsageView.as_view(), name="usage"),
