60ca1b41947d40439eb5bff0fdf17898d5a8a98c
TTstacks <ekosh21.04.2007@gmail.com>
2026-06-09 13:24:00 +0500

    Added refresh API for jwt auth

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from api.views import JobCreateView, JobDetailView, LoginView, RegisterView, UsageView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("auth/register", RegisterView.as_view(), name="auth-register"),
     path("auth/login", LoginView.as_view(), name="auth-login"),
+    path("aut/refresh", TokenRefreshView.as_view(), name="auth-refresh"),
     path("jobs", JobCreateView.as_view(), name="job-create"),
     path("jobs/<int:pk>", JobDetailView.as_view(), name="job-detail"),
     path("me/usage", UsageView.as_view(), name="usage"),
